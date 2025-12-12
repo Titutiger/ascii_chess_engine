cbb4cc495569be23db28ea241c3ce3d2fe23c526
Titutiger <99133835+Titutiger@users.noreply.github.com>
2025-12-12 21:35:31 +0530

    Update board.py

--- board.py
+++ board.py
@@ -15,16 +15,37 @@ Using these to form the grid
 #    os.system('cls')
 #clrscr()
 
-print("\n         A        B        C        D        E        F        G        H")
-print("     "+"┏━━━━━━━━"+"┳━━━━━━━━"*7+"┓")
-print("     "+"┃        "*8+"┃")
-print("  1  "+"┃        "*8+"┃")    
-print("     "+"┃        "*8+"┃")
-i = 0
-while i < 7:
-    print("     "+"┣━━━━━━━━" + "╋━━━━━━━━"*7+"┫")
-    print("     "+"┃        "*8+"┃")
-    print("  "+str(i+2)+"  "+"┃        "*8+"┃    ")    
-    print("     "+"┃        "*8+"┃")
-    i += 1
-print("     "+"┗━━━━━━━━" + "┻━━━━━━━━"*7+"┛")
+def board(style: str = 'solid') -> str:
+    if style == 'solid':
+        print("\n         A        B        C        D        E        F        G        H")
+        print("     "+"┏━━━━━━━━"+"┳━━━━━━━━"*7+"┓")
+        print("     "+"┃        "*8+"┃")
+        print("  1  "+"┃        "*8+"┃")
+        print("     "+"┃        "*8+"┃")
+        i = 0
+        while i < 7:
+            print("     "+"┣━━━━━━━━" + "╋━━━━━━━━"*7+"┫")
+            print("     "+"┃        "*8+"┃")
+            print("  "+str(i+2)+"  "+"┃        "*8+"┃    ")
+            print("     "+"┃        "*8+"┃")
+            i += 1
+        print("     "+"┗━━━━━━━━" + "┻━━━━━━━━"*7+"┛")
+
+    elif style == 'pipe':
+        print("\n         A        B        C        D        E        F        G        H")
+        print("     " + "╔════════" + "╔════════" * 7 + "┓")
+        print("     " + "║        " * 8 + "║")
+        print("  1  " + "║        " * 8 + "║")
+        print("     " + "║        " * 8 + "║")
+        i = 0
+        while i < 7:
+            print("     " + "╫════════" + "╬════════" * 7 + "┫")
+            print("     " + "║        " * 8 + "║")
+            print("  " + str(i + 2) + "  " + "║        " * 8 + "║    ")
+            print("     " + "║        " * 8 + "║")
+            i += 1
+        print("     " + "╚════════" + "╨════════" * 7 + "┛")
+
+if __name__ == '__main__':
+    board(style='pipe')
+    board()
