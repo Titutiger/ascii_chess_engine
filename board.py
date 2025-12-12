e034ad939a592591438a48044f22015eedc9266e
ojaskanitkar115-lab <ojaskanitkar115@gmail.com>
2025-12-12 19:59:09 +0530

    Create board.py

---
+++ board.py
@@ -0,0 +1,30 @@
+# base grid layout(not including any variables)
+"""
+Charchters for grid
+┌ ┐ └ ┘ ─ │ ├ ┤ ┬ ┴ ┼
+╔ ╗ ╚ ╝ ═ ║ ╠ ╣ ╦ ╩ ╬
+╒ ╓ ╕ ╖ ╘ ╙ ╛ ╜ ╞ ╟ ╡ ╢
+╤ ╥ ╧ ╨ ╪ ╫
+━ ┃ ┏ ┓ ┗ ┛ ┣ ┫ ┳ ┻ ╋
++ - |
+Using these to form the grid
+"""
+import os
+
+def clrscr():
+    os.system('cls')
+clrscr()
+print("\n         A        B        C        D        E        F        G        H")    
+print("     "+"┏━━━━━━━━"+"┳━━━━━━━━"*7+"┓")
+print("     "+"┃        "*8+"┃")
+print("  1  "+"┃        "*8+"┃")    
+print("     "+"┃        "*8+"┃")
+i = 0
+while i < 7:
+    print("     "+"┣━━━━━━━━" + "╋━━━━━━━━"*7+"┫")
+    print("     "+"┃        "*8+"┃")
+    print("  "+str(i+2)+"  "+"┃        "*8+"┃    ")    
+    print("     "+"┃        "*8+"┃")
+    i += 1
+print("     "+"┗━━━━━━━━" + "┻━━━━━━━━"*7+"┛")
+#the end
