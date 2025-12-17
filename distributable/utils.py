594701d07f96c2bfa0c6dcf05e6dd41963b5ec61
Titutiger <99133835+Titutiger@users.noreply.github.com>
2025-12-17 17:33:25 +0530

    changed distri

--- distributable/utils.py
+++ distributable/utils.py
@@ -1,6 +1,13 @@
 import shutil
 from typing import Tuple
 
+title: str = 'TermiChess'
+authors: str = 'Aarin & Ojas'
+lic: str = 'Null'
+
+inception: str = 'Null'
+v_dev: str = '1.0.0'
+v_usr: str = '1.0'
 
 def get_terminal_size() -> Tuple[int, int]:
     """
